a37e1168371451a8f6b1914853a9e6df22ab1f8a
Your Name <nielsdegroot@live.be>
2020-09-08 12:45:00 +0200

    blijft nog altijd loopen

--- advanced_/brainfuck/main.js
+++ advanced_/brainfuck/main.js
@@ -3,11 +3,12 @@ function interpretBrainFuck(program, inputArray, arrayLength) {
   let filteredArray = filterArray(program);
   let locationIndex = 0;
   let index = 0;
-  let resultArray = 0;
+  let resultArray = "";
 
   while (locationIndex < filteredArray.length) {
     switch (program[locationIndex]) {
       case "+":
+        if (mainArray[index] === 255) return "INCORRECT VALUE";
         mainArray[index]++;
         locationIndex++;
         console.log(locationIndex);
@@ -15,16 +16,19 @@ function interpretBrainFuck(program, inputArray, arrayLength) {
         break;
 
       case "-":
+        if (mainArray[index] === 0) return "INCORRECT VALUE";
         mainArray[index]--;
         locationIndex++;
         break;
 
       case "<":
+        if (index === 0) return 'POINTER OUT OF BOUNDS';
         index--;
         locationIndex++;
         break;
 
       case ">":
+        if (index === arrayLength - 1) return 'POINTER OUT OF BOUNDS';
         index++;
         locationIndex++;
         break;
