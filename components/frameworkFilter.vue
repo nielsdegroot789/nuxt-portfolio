b2f34146c91eaa89ab3f6bfbc3155f4d42c56a8b
Your Name <nielsdegroot@live.be>
2020-09-14 14:44:43 +0200

    test

---
+++ components/frameworkFilter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="filter-container">
+    <label for=""></label><input type="checkbox" name="" id="" />
+    <label for=""></label><input type="checkbox" name="" id="" />
+    <label for=""></label><input type="checkbox" name="" id="" />
+    <label for=""></label><input type="checkbox" name="" id="" />
+  </div>
+</template>
+
+<script>
+export default {
+    name:"filter"
+}
+</script>
+
+<style></style>
