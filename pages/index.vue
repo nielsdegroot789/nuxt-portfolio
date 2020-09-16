d59cd75694e213a8724970175380f0682a663fb6
Your Name <nielsdegroot@live.be>
2020-09-16 21:21:50 +0200

    final commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,111 @@
 <template>
-  <div class="container">
-    <frameworkFilter />
+  <div>
+    <div class="results-container">
+      <div v-for="(item, index) in people.results" :key="index" class="results">
+        {{ item.name }}
+      </div>
+    </div>
+    <div class="container-2">
+      <li
+        v-for="item in items"
+        :key="item.id"
+        :class="{ active: selected == item.id }"
+        @click="getValue(item.id)"
+      >
+        {{ item.id }}
+      </li>
+    </div>
+    <input type="button" value="Go Back" class="back" @click="Back" />
   </div>
 </template>
 
 <script>
-import frameworkFilter from '../components/frameworkFilter';
-
 export default {
-  components: {
-    frameworkFilter,
+  data() {
+    return {
+      isActive: false,
+      selected: [],
+      items: [],
+      goBack: true,
+    };
   },
   computed: {
     people() {
-      return this.$store.state.people;
+      return this.$store.getters.people;
+    },
+    secondIndex() {
+      return this.$store.state.index[1];
     },
   },
   created() {
     this.$store.dispatch('loadPeople');
+    this.generateLinks();
+  },
+  methods: {
+    generateLinks() {
+      for (let i = 1; i < 10; i++) {
+        this.items.push({
+          id: i,
+        });
+      }
+    },
+
+    getValue(id) {
+      this.selected = id;
+      this.$store.dispatch('changePage', id);
+    },
+    Back() {
+      const index = this.secondIndex;
+      this.selected = index;
+      this.$store.dispatch('goBack', index);
+    },
   },
 };
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
+.results-container {
+  height: 320px;
+}
+input[type='button'] {
+  background-color: rgb(149, 201, 149);
+  border-radius: 3px;
+  width: 5rem;
+  height: 2rem;
+  display: block;
+  margin: auto;
+  width: 50%;
+}
+.results {
+  margin-top: 10px;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
+  list-style: none;
 }
 
+.container-2 {
+  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  list-style: none;
+}
+
+li {
+  padding: 1rem;
+  cursor: pointer;
+  border-radius: 3px;
+}
+.active {
+  border: 5px solid goldenrod;
+}
+.navigation {
+  text-align: center;
+  cursor: pointer;
+}
 .title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
--- store/index.js
+++ store/index.js
@@ -1,17 +1,46 @@
 export const state = () => ({
   people: [],
+  index: [],
 });
 
 export const actions = {
   async loadPeople({ commit }) {
     await this.$axios.get('https://swapi.dev/api/people/').then((response) => {
-      commit('swapi', response.data);
+      commit('swapiPage', response.data);
     });
+  },
+
+  async changePage({ commit }, index) {
+    await this.$axios
+      .get('https://swapi.dev/api/people/?page=' + index)
+      .then((response) => {
+        commit('swapiPage', response.data);
+      });
+    commit('saveIndex', index);
+  },
+
+  async goBack({ commit }, index) {
+    await this.$axios
+      .get('https://swapi.dev/api/people/?page=' + index)
+      .then((response) => {
+        commit('swapiPage', response.data);
+      });
+    commit('deleteIndex');
   },
 };
 
 export const mutations = {
-  swapi(state, people) {
-    state.people = people;
+  swapiPage(state, response) {
+    state.people = response;
   },
+  saveIndex(state, index) {
+    state.index.unshift(index);
+  },
+  deleteIndex(state) {
+    state.index.shift(); // hier map gebruiken
+  },
+};
+
+export const getters = {
+  people: (state) => state.people,
 };
